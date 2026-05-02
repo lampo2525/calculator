ca8459d5c7ed0bf044108a8f7ba26e79aca3b1d5
developer2 <lampo0807@gmail.com>
2026-05-02 10:49:35 +0900

    Add sum function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -2,7 +2,12 @@ class Control:
     def __init__(self, view):
         self.view = view
         self.connectSignals()
-        
+    def calculate(self):
+        pass
+            
     def connectSignals(self):
         self.view.btn1.clicked.connect(self.view.activateMessage)
         self.view.btn2.clicked.connect(self.view.clearMessage)
+
+    def sum(self, a, b): # 덧셈 함수 추가
+        return a+b
